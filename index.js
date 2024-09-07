acbd7f5befe2012b4cc711b4f56567af85abafb3
premkumar-004 <premkumarkardale@gmail.com>
2024-09-07 20:20:33 +0530

    a basic styling added and index.ejs page rendered

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = 8080;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 main().then((res) => {
     console.log("Connection Successful");
@@ -19,6 +20,12 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
 
+//Index Route
+app.get("/chats", async (req, res) => {
+    let chats = await Chat.find();
+    // console.log(chats);
+    res.render("index.ejs", { chats });
+})
 
 app.get("/", (req, res) => {
     res.send("Home");
